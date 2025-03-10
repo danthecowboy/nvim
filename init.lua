c9ff813209d933023e24cf4ac22db4a3fa013941
Daniel Haberkorn <daniel@binarypeople.de>
2025-03-10 22:19:28 +0100

    scheme and lsp

--- init.lua
+++ init.lua
@@ -658,6 +658,9 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        helm_ls = {},
+        yamlls = {},
+        eslint = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -889,20 +892,20 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
+      require('catppuccin').setup {
+        -- styles = {
+        --  comments = { italic = false }, -- Disable italics in comments
+        -- },
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
